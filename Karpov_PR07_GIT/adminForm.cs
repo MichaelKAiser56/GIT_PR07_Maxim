0834aea7aa7cfdbb0666c399cda22e0e364135ee
Karpov Maksim Vladimirovich <mkarpov648@gmail.com>
2025-10-20 21:21:45 +0300

    Добавлена форма Администратора и добавлены базовые элементы

---
+++ Karpov_PR07_GIT/adminForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Karpov_PR07_GIT
+{
+    public partial class adminForm : Form
+    {
+        public adminForm()
+        {
+            InitializeComponent();
+
+            this.pictureBox1.Image = Properties.Resources.logo;
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void adminForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
